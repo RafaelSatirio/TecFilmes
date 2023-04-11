43d9a8477e2d187813e93bc1e9c19b08786bdecf
RafaelSatirio <125366637+RafaelSatirio@users.noreply.github.com>
2023-04-11 11:47:15 -0300

    Add files via upload

---
+++ src/data/movies.js
@@ -0,0 +1,63 @@
+const DATA =
+[
+    {
+    id: '01',
+    nome: 'Adão Negro',
+    nota:'8.8',
+    imagem: '1.jpg'
+    },
+    {
+    id: '02',
+    nome: 'John Wick 4',
+    nota:'8.8',
+    imagem: '2.jpg'
+    },
+    {
+    id: '03',
+    nome: 'Shazam! Fúria dos Deuses',
+    nota:'8.8',
+    imagem: '3.jpg'
+    },
+    {
+    id: '04',
+    nome: 'Pânico 6',
+    nota:'8.8',
+    imagem: '4.jpg'
+    },
+    {
+    id: '05',
+    nome: 'Demon Slayer - Para a Vila do Espadachim',
+    nota:'8.8',
+    imagem: '5.jpg'
+    },
+    {
+    id: '06',
+    nome: 'Super Mario Bros. O Filme',
+    nota:'8.8',
+    imagem: '6.jpg'
+    },
+    {
+    id: '07',
+    nome: 'Dungeons e Dragons',
+    nota:'8.8',
+    imagem: '7.jpg'
+    },
+    {
+    id: '08',
+    nome: 'A Primeira Comunhão',
+    nota:'8.8',
+    imagem: '8.jpg'
+    },
+    {
+    id: '09',
+    nome: 'Gato de Botas 2',
+    nota:'8.8',
+    imagem: '9.jpg'
+    },
+    {
+    id: '10',
+    nome: 'Creed 3',
+    nota:'8.8',
+    imagem: '10.jpg'
+    },
+]
